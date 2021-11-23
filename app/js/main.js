ab25cfaeed86d14cce92fa0515cbda8d64c68821
vladk7072 <vladk7072@gmail.com>
2021-11-23 13:25:49 +0200

    index request

---
+++ app/js/main.js
@@ -0,0 +1,43 @@
+$(function () {
+  const swiperTop = new Swiper(".top__content", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    navigation: {
+      nextEl: ".top__btn-next",
+      prevEl: ".top__btn-prev",
+    },
+  });
+
+  var mySliderAllSlides = document.querySelector(".top__total"),
+    mySliderCurrentSlides = document.querySelector(".top__current");
+
+  mySliderAllSlides.innerHTML = "/0" + swiperTop.slides.length;
+
+  swiperTop.on("slideChange", function () {
+    var currentSlide = ++swiperTop.realIndex;
+    mySliderCurrentSlides.innerHTML = "0" + currentSlide;
+  });
+
+  const title = document.querySelector(".about__title"),
+    titleBg = document.querySelector(".about__title-bg");
+
+  titleBg.innerHTML = title.innerHTML;
+
+  const title2 = document.querySelector(".category__title"),
+    titleBg2 = document.querySelector(".category__title-bg");
+
+  titleBg2.innerHTML = title2.innerHTML;
+
+  var mixer = mixitup(".category__items");
+  
+  const tripBar = document.querySelector(".category__bar");
+  tripBar.addEventListener("click", (e) => {
+    $(".category__bar-item--active").removeClass("category__bar-item--active");
+    e.target.classList.add("category__bar-item--active");
+  });
+
+});
